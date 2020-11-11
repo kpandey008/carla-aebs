27bcfdfa85496b127172fc2f1b6b3b34c1268128
Kushagra Pandey <kpandeyce008@gmail.com>
2020-11-11 20:42:36 +0530

    added command to train the perception lec

--- train_perception.py
+++ train_perception.py
@@ -1,3 +1,4 @@
+import click
 import torchvision.transforms as T
 
 from perception.trainer import Trainer
@@ -5,17 +6,22 @@ from perception.dataset import PerceptionDataset
 from perception.model import PerceptionNet
 
 
-base_dir = '/home/lexent/carla_simulation/perception_data/'
-save_path = '/home/lexent/carla_simulation/chkpt/'
-seed = 0
+@click.argument(base_dir)
+@click.argument(save_path)
+@click.option('--random-state', default=0, type=int)
+@click.command()
+def train_perception(base_dir, save_path, random_state=0):
+    # Define the transforms
+    transform = T.Compose([
+        T.ToTensor(),
+    ])
+    train_dataset = PerceptionDataset(base_dir, seed=seed, mode='train', transform=transform)
+    val_dataset = PerceptionDataset(base_dir, seed=seed, mode='val', transform=transform)
+    model = PerceptionNet()
 
-# Define the transforms
-transform = T.Compose([
-    T.ToTensor(),
-])
-train_dataset = PerceptionDataset(base_dir, seed=seed, mode='train', transform=transform)
-val_dataset = PerceptionDataset(base_dir, seed=seed, mode='val', transform=transform)
-model = PerceptionNet()
+    trainer = Trainer(train_dataset, val_dataset, model, random_state=seed)
+    trainer.train(save_path)
 
-trainer = Trainer(train_dataset, val_dataset, model, random_state=seed)
-trainer.train(save_path)
+
+if __name__ == '__main__':
+    train_perception()
